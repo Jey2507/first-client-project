6ec67ef5f3414d19a973ca7f745798be388d1697
Oleh Toporovych <otoporovych2@gmail.com>
2024-10-26 15:31:25 +0200

    add

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import css from "../Hero/Hero.module.css"
 import imagePath from "../../assets/images/about/ania_one_tablet.jpg";
+import imagePathDesktop from "../../assets/images/about/ania_one_desc.jpg";
 
 
 export default function Hero() {
@@ -15,6 +16,7 @@ export default function Hero() {
                 </div>
             </div>
             <img className={css.image} src={imagePath} alt="ania" />
+            <img className={css.imageDesc} src={imagePathDesktop} alt="ania" />
         </section>
     )
 }
