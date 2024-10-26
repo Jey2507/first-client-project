24a0b0a2413f0c680640296152b2fa50a1508efb
Oleh Toporovych <otoporovych2@gmail.com>
2024-10-26 02:34:27 +0200

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,6 @@
 import css from "../Hero/Hero.module.css"
+import imagePath from "../../assets/images/about/ania_one_tablet.jpg";
+
 
 export default function Hero() {
     return (
@@ -12,7 +14,7 @@ export default function Hero() {
                     <button className={css.heroButtonTwo}>Dołącz do kursu</button>
                 </div>
             </div>
-            <img className={css.image} src="/assets/images/about/ania_one_tablet.jpg" alt="ania" />
+            <img className={css.image} src={imagePath} alt="ania" />
         </section>
     )
 }
