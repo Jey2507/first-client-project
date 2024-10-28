369d2b7d719d366f11fd05214423140c0db1bb19
Oleh Toporovych <otoporovych2@gmail.com>
2024-10-28 17:52:08 +0100

    update hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,15 +7,15 @@ export default function Hero() {
     return (
         <section className={css.divHero}>
             <div>
-                <h1 className={css.heroText}>Naucz się tworzyć rolki, dzięki którym zwiększysz swój <span className={css.spanText}>dochód x2</span></h1>
+                <h1 className={css.heroText}>Naucz się tworzyć rolki, dzięki którym<br /> zwiększysz swój <span className={css.spanText}>dochód x2</span></h1>
                 <div className={css.divBack}>
                     <p className={css.heroDescr}>Hej, jestem Ania i ja napewno wiem jak zrobić ze by twój content przynosił Ci większy dochód.</p>
                 </div>
                
 
                 <div className={css.divButton}>
-                    <a className={css.heroButtonOne} href="#aboutMe">Dowiedz się więcej</a>
-                    <a className={css.heroButtonTwo}>Dołącz do kursu</a>
+                    <a className={css.heroButton} href="#aboutMe">Dowiedz się więcej</a>
+                    <a className={css.heroButton}>Dołącz do kursu</a>
                 </div>
             </div>
             <img className={css.image} src={imagePath} alt="ania" />
