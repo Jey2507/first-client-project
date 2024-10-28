771fe7e5f49ccd7c0634eab6bbaea6ddc221d390
Oleh Toporovych <otoporovych2@gmail.com>
2024-10-28 18:04:15 +0100

    update mainText

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ export default function Hero() {
     return (
         <section className={css.divHero}>
             <div>
-                <h1 className={css.heroText}>Naucz się tworzyć rolki, dzięki którym<br /> zwiększysz swój <span className={css.spanText}>dochód x2</span></h1>
+                <h1 className={css.heroText}>Naucz się <br /> tworzyć rolki,<br /> dzięki którym<br /> zwiększysz swój <span className={css.spanText}>dochód x2</span></h1>
                 <div className={css.divBack}>
                     <p className={css.heroDescr}>Hej, jestem Ania i ja napewno wiem jak zrobić ze by twój content przynosił Ci większy dochód.</p>
                 </div>
