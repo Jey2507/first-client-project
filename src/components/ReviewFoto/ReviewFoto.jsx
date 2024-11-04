8a8d653cde136ea4d3fbb9110f8b364c80e3e1b1
Oleh Toporovych <otoporovych2@gmail.com>
2024-11-04 18:11:43 +0100

    finish reviews section

--- src/components/ReviewFoto/ReviewFoto.jsx
+++ src/components/ReviewFoto/ReviewFoto.jsx
@@ -1,57 +1,67 @@
-import { useRef, useEffect, useState } from "react";
+import { useRef, useEffect } from "react";
 import Swiper from "swiper";
 import "swiper/css";
 import clsx from "clsx";
 import css from "../ReviewFoto/ReviewFoto.module.css";
-import imageOne from "../../assets/images/reviews/review_four_desc.png";
-import imageTwo from "../../assets/images/reviews/review_five_desc.png";
+import imageOne from "../../assets/images/reviews/review_one.png";
+import imageTwo from "../../assets/images/reviews/review_two.png";
+import imageThree from "../../assets/images/reviews/review_three.png";
+import imageFour from "../../assets/images/reviews/review_four.png";
+import imageFive from "../../assets/images/reviews/review_five.png";
+import imageSix from "../../assets/images/reviews/review_six.png";
+import imageSeven from "../../assets/images/reviews/review_seven.png";
+import imageEigth from "../../assets/images/reviews/review_eigth.png";
+import imageNine from "../../assets/images/reviews/review_nine.png";
+import imageTen from "../../assets/images/reviews/review_ten.png";
+import imageEleven from "../../assets/images/reviews/review_eleven.png";
+import imageTwel from "../../assets/images/reviews/review_twel.png";
+import imageThird from "../../assets/images/reviews/review_thrid.png";
+import imageFourteen from "../../assets/images/reviews/review_fourteen.png";
+import imageFiveteen from "../../assets/images/reviews/review_fifteen.png";
+import imageSixteen from "../../assets/images/reviews/review_sexteen.png";
+import { icons as sprite } from '../../assets/images/index.js';
 
 const ReviewFoto = () => {
   const prevButtonRef = useRef(null);
   const nextButtonRef = useRef(null);
   const swiperContainerRef = useRef(null);
-  const [swiperInstance, setSwiperInstance] = useState(null);
+  const swiperRef = useRef(null); 
 
   useEffect(() => {
-    // Initialize Swiper
-    const swiper = new Swiper(swiperContainerRef.current, {
-      preventInteractionOnTransition: true,
-      navigation: {
-        nextEl: nextButtonRef.current,
-        prevEl: prevButtonRef.current,
-      },
-    });
+    if (prevButtonRef.current && nextButtonRef.current && swiperContainerRef.current && !swiperRef.current) {
+      swiperRef.current = new Swiper(swiperContainerRef.current, {
+        slidesPerView: 4,
+        slidesPerGroup: 1,
+        spaceBetween: 16,
+        navigation: {
+          nextEl: nextButtonRef.current,
+          prevEl: prevButtonRef.current,
+        },
+        breakpoints: {
+          320: { slidesPerView: 1, spaceBetween: 16 },
+          768: { slidesPerView: 2, spaceBetween: 16 },
+          1440: { slidesPerView: 4, spaceBetween: 16 },
+        },
+      });
 
-    setSwiperInstance(swiper);
+      const updateButtonStates = () => {
+        if (prevButtonRef.current && nextButtonRef.current) {
+          prevButtonRef.current.disabled = swiperRef.current.isBeginning;
+          nextButtonRef.current.disabled = swiperRef.current.isEnd;
+        }
+      };
 
-    // Disable the previous button initially
-    if (prevButtonRef.current) prevButtonRef.current.disabled = true;
-
-    // Event listener to enable/disable buttons on slide change
-    swiper.on("slideChange", () => {
-      if (nextButtonRef.current) {
-        nextButtonRef.current.disabled = swiper.isEnd;
-      }
-      if (prevButtonRef.current) {
-        prevButtonRef.current.disabled = swiper.isBeginning;
-      }
-    });
+      updateButtonStates();
+      swiperRef.current.on("slideChange", updateButtonStates);
+    }
 
     return () => {
-      // Clean up Swiper instance on component unmount
-      swiper.destroy();
+      if (swiperRef.current) {
+        swiperRef.current.destroy();
+        swiperRef.current = null;
+      }
     };
   }, []);
-
-  const handlePrevClick = () => {
-    if (swiperInstance) swiperInstance.slidePrev();
-  };
-
-  const handleNextClick = () => {
-    if (swiperInstance) swiperInstance.slideNext();
-  };
-
-  console.log(css.swipeBtnProjects); // Має вивести згенероване ім'я класу або `undefined`
 
   return (
     <section className={css.section} id="projects">
@@ -59,41 +69,30 @@ const ReviewFoto = () => {
         <h2 className={css.sectionsTitle}>Opinii</h2>
         <div className="swiper swiper-projects" ref={swiperContainerRef}>
           <ul className={clsx(css.projectsList, "swiper-wrapper")}>
-            <li className={clsx(css.projectsListItem, "swiper-slide")}>
-              <img src={imageOne} alt="firstFoto" />
-            </li>
-            <li className={clsx(css.projectsListItem, "swiper-slide")}>
-              <img src={imageTwo} alt="secondFoto" />
-            </li>
+            {[imageOne, imageTwo, imageThree, imageFour, imageFive, imageSix, imageSeven, imageEigth, imageNine, imageTen, imageEleven, imageTwel, imageThird, imageFourteen, imageFiveteen, imageSixteen].map((image, index) => (
+              <li key={index} className={clsx(css.projectsListItem, "swiper-slide")}>
+                <img className={css.imageItem} src={image} alt={`photo-${index}`} />
+              </li>
+            ))}
           </ul>
           <div className={css.buttonsThumb}>
             <button
-              className={clsx(
-                css.swipeBtnProjects,
-                "swiper-button-prev",
-                "pr-btn-prev"
-              )}
+            onClick={() => {swiperRef.current.slidePrev()}}
+              className={clsx(css.swipeBtnProjects, "swiper-button-prev")}
               type="button"
-              ref={prevButtonRef}
-              onClick={handlePrevClick}>
+              ref={prevButtonRef}>
               <svg className={clsx(css.btnSvg, css.btnSvgRight)}>
-                <use href="./img/icons.svg#icon-arrow-narrow-right" />
+                <use xlinkHref={`${sprite}#icon-arrow-narrow-right`} />
               </svg>
             </button>
             <button
-              className={clsx(
-                css.swipeBtnProjects,
-                "swiper-button-next",
-                "pr-btn-next"
-              )}
+              onClick={() => {swiperRef.current.slideNext()}}
+              className={clsx(css.swipeBtnProjects, "swiper-button-next")}
               type="button"
-              ref={nextButtonRef}
-              onClick={handleNextClick}>
+              ref={nextButtonRef}>
+              
               <svg className={css.btnSvg}>
-                <use
-                  className={css.iconArrow}
-                  href="./img/icons.svg#icon-arrow-narrow-right"
-                />
+              <use xlinkHref={`${sprite}#icon-arrow-narrow-right`} />
               </svg>
             </button>
           </div>
